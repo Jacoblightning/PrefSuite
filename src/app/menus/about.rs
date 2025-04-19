09a0f28b6617231dd4cf8b1ba2452f7158aaadda
Jacob Freeman <t00niyf5@duck.com>
2025-04-19 11:07:13 -0400

    Re-License under GPL (Because AGPL makes no sense for this)

--- src/app/menus/about.rs
+++ src/app/menus/about.rs
@@ -1,3 +1,21 @@
+/*
+    PrefSuite. A Preferences suite for MacOS
+    Copyright (C) 2025-Present Jacob (https://github.com/jacoblightning)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 use crate::app::{MyApp, Menu};
 
 
@@ -15,5 +33,8 @@ pub fn main(app: &mut MyApp, ctx: &egui::Context) {
         ui.label("PrefSuite is a WIP app for modifying MacOS system preferences.");
         ui.label("I made this app because I was looking to make MacOS more like Linux. Unlike what everyone else is trying to do.");
         ui.label("Hopefully in the future, it can also be an open-source reference to the settings of MacOS.");
+        ui.add_space(60.0);
+        ui.label("This program is licensed under the GPL:");
+        ui.label("    This program is free software: you can redistribute it and/or modify\n    it under the terms of the GNU General Public License as published by\n    the Free Software Foundation, either version 3 of the License, or\n    (at your option) any later version.\n\n    This program is distributed in the hope that it will be useful,\n    but WITHOUT ANY WARRANTY; without even the implied warranty of\n    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n    GNU General Public License for more details.\n\n    You should have received a copy of the GNU General Public License\n    along with this program.  If not, see <https://www.gnu.org/licenses/>.");
     });
 }
