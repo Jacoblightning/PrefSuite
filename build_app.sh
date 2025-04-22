8559117a916e4d059683e42d7708d07994a5c535
Jacob Freeman <t00niyf5@duck.com>
2025-04-21 21:05:16 -0400

    Build Script

---
+++ build_app.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <Directory to put app>"
+  exit
+fi
+
+cargo build --release
+
+mkdir -p "$1/PrefSuite.app/Contents/MacOS/"
+
+cp target/release/PrefSuite "$1/PrefSuite.app/Contents/MacOS"
+
+echo "App: $1/PrefSuite.app"
